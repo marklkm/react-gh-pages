72c68af5008d63ccf329132dc463504e09058d5f
mark likeman <mark.likeman@hotmail.com>
2023-01-06 15:06:19 -0600

    updated App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-//import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
     return ( 
       <h1> Hello World! </h1>
-      <h2> Hello World! </h2>
     );
 }
 
